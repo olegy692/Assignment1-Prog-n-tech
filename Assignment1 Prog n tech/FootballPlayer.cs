75d08c000fa3b7ed5e984e24ca07547a8d9260e9
olegs <olegs@LAPTOP-IIRSH5S9>
2022-10-13 10:01:26 +0200

    Add project files.

---
+++ Assignment1 Prog n tech/FootballPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_Prog_n_tech
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Name + " " + Age + " " + ShirtNumber;
+        }
+
+        public void NameValidator()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name should not be null");
+
+            }
+
+            if (Name.Length < 2)
+            {
+                throw new ArgumentException("Name should be at lease 2 characters long");
+
+            }
+        }
+
+        public void AgeValidator()
+        {
+            if(Age <= 1)
+            {
+                throw new ArgumentOutOfRangeException("Age should be more than 1");
+            }
+        }
+
+
+        public void ShirtNumberValidator()
+        {
+            if(ShirtNumber<=1 || ShirtNumber<=99)
+            {
+                throw new ArgumentOutOfRangeException("Shirt number should be between 1 and 99");
+            }
+        }
+
+
+        public void Validate()
+        {
+            NameValidator();
+            AgeValidator();
+            ShirtNumberValidator();
+
+        }
+    }
+
+    
+}
---
+++ Assignment1 Prog n techTests1/FootballPlayerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment1_Prog_n_tech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_Prog_n_tech.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+
+       
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            FootballPlayer player = new FootballPlayer() { Name = "Vincent", ShirtNumber = 39, Age = 26, Id = 3926 };
+            string str = player.ToString();
+            Assert.AreEqual(str, player.ToString());
+
+        }
+        [TestMethod()]
+        public void NameValidatorTest()
+        {
+          
+            FootballPlayer player = new FootballPlayer() { Name = "", ShirtNumber = 39, Age = 26, Id = 3926 };
+            Assert.ThrowsException<ArgumentException>(() => player.NameValidator());
+
+            FootballPlayer player1 = new FootballPlayer() { Name = null, ShirtNumber = 39, Age = 26, Id = 3926 };
+            Assert.ThrowsException<ArgumentNullException>(() => player1.NameValidator());
+
+        }
+
+
+        [TestMethod()]
+        public void AgeValidatorTest()
+        {
+
+            FootballPlayer player = new FootballPlayer() { Name = "Vincent", ShirtNumber = 39, Age = 1, Id = 3926 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player.AgeValidator());
+
+           
+        }
+
+        [TestMethod()]
+        public void ShirtNumberValidatorTest()
+        {
+            FootballPlayer player = new FootballPlayer() { Name = "Vincent", ShirtNumber = 0, Age = 0, Id = 3926 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player.ShirtNumberValidator());
+
+            FootballPlayer player1 = new FootballPlayer() { Name = "Vincent", ShirtNumber = 100, Age = 0, Id = 3926 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player.ShirtNumberValidator());
+
+            FootballPlayer player2 = new FootballPlayer() { Name = "Vincent", ShirtNumber = -1, Age = 0, Id = 3926 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player.ShirtNumberValidator());
+
+
+        }
+
+       
+    }
+    }
